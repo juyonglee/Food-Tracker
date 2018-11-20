ad58394f754a42e902b2d24100c298b05563289d
juyonglee <juyonglee0208@gmail.com>
2018-11-20 23:19:39 +0900

    Food Tracker 프로젝트 생성

---
+++ Food Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Juyong Lee on 20/11/2018.
+//  Copyright © 2018 Juyong Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Food TrackerTests/Food_TrackerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Food_TrackerTests.swift
+//  Food TrackerTests
+//
+//  Created by Juyong Lee on 20/11/2018.
+//  Copyright © 2018 Juyong Lee. All rights reserved.
+//
+
+import XCTest
+@testable import Food_Tracker
+
+class Food_TrackerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

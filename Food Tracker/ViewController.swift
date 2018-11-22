55f2f2d417171d4b49406928f9b00f4442d66196
juyonglee <juyonglee0208@gmail.com>
2018-11-23 03:16:43 +0900

    TextFiled와 Label에 대한 Outlet 생성

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     //  MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()

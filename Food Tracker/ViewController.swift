96d3ab9d9e196b38e2dba2bc699786e85db47094
juyonglee <juyonglee0208@gmail.com>
2018-11-23 04:36:23 +0900

    Action method 추가

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    //  MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    }
 }
 

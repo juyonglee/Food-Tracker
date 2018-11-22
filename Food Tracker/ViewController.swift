e09275c8f3079e7c187692dcfca09302809672b4
juyonglee <juyonglee0208@gmail.com>
2018-11-23 02:16:44 +0900

    특별한 타입의 주석인 MARK를 추가

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //  MARK: Properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

1a73343ea8332667159fb2890f62295811adeb3b
juyonglee <juyonglee0208@gmail.com>
2018-12-02 02:12:27 +0900

    Custom view를 위한 Class 생성

---
+++ Food Tracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  Food Tracker
+//
+//  Created by Juyong Lee on 02/12/2018.
+//  Copyright © 2018 Juyong Lee. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

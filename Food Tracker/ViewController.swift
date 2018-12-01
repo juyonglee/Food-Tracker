ac896d76a2c079bc49a2be2779d5f1009c9f6c50
juyonglee <juyonglee0208@gmail.com>
2018-12-02 00:59:26 +0900

    UIImagePickerController Delegate method 구현

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -51,5 +51,24 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     func textFieldDidEndEditing(_ textField: UITextField) {
         mealNameLabel.text = textField.text
     }
+    
+    //  MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        //  Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //  The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        //  Set photoImageView to display the selected image.
+        photoImageView.image = selectedImage
+        
+        //  Dissmiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
 }
 

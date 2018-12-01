fa1669e95b1cf116d9001bf083747e3c392f2329
juyonglee <juyonglee0208@gmail.com>
2018-12-02 00:17:08 +0900

    UIImagePickerControllerDelegate 추가

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate {
     
     //  MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!

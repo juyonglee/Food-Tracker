43ad51417d10ed43ca50129e223abaf9fa640104
juyonglee <juyonglee0208@gmail.com>
2018-12-02 14:41:32 +0900

    Class initializer 추가

--- Food Tracker/RatingControl.swift
+++ Food Tracker/RatingControl.swift
@@ -10,12 +10,13 @@ import UIKit
 
 class RatingControl: UIStackView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //  MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
-
+    
+    //  required 수식어를 Class Initializer 앞에 정의하여 해당 Class의 모든 Class는 Initializer를 구현해야 한다고 표시한다.
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 }

6687def3e274997d8138bbd524aafd0820a4f15d
juyonglee <juyonglee0208@gmail.com>
2018-12-03 01:00:34 +0900

    Button을 추가하는 setupButtons Method 생성

--- Food Tracker/RatingControl.swift
+++ Food Tracker/RatingControl.swift
@@ -13,10 +13,27 @@ class RatingControl: UIStackView {
     //  MARK: Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
     
     //  required 수식어를 Class Initializer 앞에 정의하여 해당 Class의 모든 Class는 Initializer를 구현해야 한다고 표시한다.
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //  MARK: Private Methods
+    private func setupButtons() {
+        //  Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        //  Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        //  Add the button to the stack
+        addArrangedSubview(button)
     }
 }

f264505692be47ee344db4f19fd1918709593fc2
juyonglee <juyonglee0208@gmail.com>
2018-12-03 01:38:14 +0900

    여러 개의 button 추가가 가능하도록 setupButtons method 수정

--- Food Tracker/RatingControl.swift
+++ Food Tracker/RatingControl.swift
@@ -33,19 +33,25 @@ class RatingControl: UIStackView {
     
     //  MARK: Private Methods
     private func setupButtons() {
-        //  Create the button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
+        for _ in 0..<5 {
+            //  Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            //  Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            //  Setup the button action
+            button.addTarget(self, action: #selector(ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            //  Add the button to the stack
+            addArrangedSubview(button)
+            
+            //  Add the new button to the rating button array
+            ratingButton.append(button)
+        }
         
-        //  Add constraints
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        //  Setup the button action
-        button.addTarget(self, action: #selector(ratingButtonTapped(button:)), for: .touchUpInside)
-        
-        //  Add the button to the stack
-        addArrangedSubview(button)
     }
 }

a042ed78c905c599bf61acc901bfc0938e013a9c
juyonglee <juyonglee0208@gmail.com>
2018-12-04 19:46:18 +0900

    Button 삭제 및 Stack View Item 배치 수정

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -23,10 +23,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
 
     //  MARK: Actions
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-    }
-    
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
         //  Hide the keyboard.
         nameTextField.resignFirstResponder()

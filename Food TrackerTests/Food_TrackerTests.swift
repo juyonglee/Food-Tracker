ebfb901bac09fa3784855af32bdf150781bf7759
juyonglee <juyonglee0208@gmail.com>
2018-12-04 21:54:37 +0900

    Functional test case인 testMealInitializationSucceeds 추가

--- Food TrackerTests/Food_TrackerTests.swift
+++ Food TrackerTests/Food_TrackerTests.swift
@@ -11,24 +11,10 @@ import XCTest
 
 class Food_TrackerTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    //  MARK: Meal Class Tests
+    //  Confirm that the Meal initializer returns a Meal object when passed valid parameters.
+    func testMealInitializationSucceeds() {
+        
     }
 
 }

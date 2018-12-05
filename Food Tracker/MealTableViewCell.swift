50a48adde819ff9a8aa48a91961e0ceb73c8bdd1
juyonglee <juyonglee0208@gmail.com>
2018-12-05 15:56:06 +0900

    Custom cell 작성을 위한 Class 생성

---
+++ Food Tracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  Food Tracker
+//
+//  Created by Juyong Lee on 05/12/2018.
+//  Copyright © 2018 Juyong Lee. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

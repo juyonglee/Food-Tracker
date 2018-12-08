5847e079b1fa9a4c91e772e27e676cfc41787191
juyonglee <juyonglee0208@gmail.com>
2018-12-08 16:22:12 +0900

    Scene 전환 전에 필요한 작업을 수행하는 prepare method 정의

--- Food Tracker/MealViewController.swift
+++ Food Tracker/MealViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import os.log
 
 class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -25,6 +26,25 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         super.viewDidLoad()
         //  Handle the text field's user input through delegate callbacks.
         nameTextField.delegate = self
+    }
+    
+    //  MARK: Navigation
+    //  This method lets you configure a view controller before it's presented.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        //  Configure the destination view controller only when the save button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os.os_log("The save button was not pressed, cancelling", log: OSLog.default, type: OSLogType.debug)
+            return
+        }
+        
+        let name = nameTextField.text ?? ""
+        let photo = photoImageView.image
+        let rating = ratingControl.rating
+        
+        //  Set the meal to be passed to MealTableViewController after unwind segue.
+        meal = Meal(name: name, photo: photo, rating: rating)
     }
 
     //  MARK: Actions

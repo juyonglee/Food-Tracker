b9a6e0779cebc6c3354e5d5745be63f05b593080
juyonglee <juyonglee0208@gmail.com>
2018-12-08 22:59:53 +0900

    Edit 화면에서의 데이터 설정을 위한 코드 작성

--- Food Tracker/MealViewController.swift
+++ Food Tracker/MealViewController.swift
@@ -27,6 +27,13 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         //  Handle the text field's user input through delegate callbacks.
         nameTextField.delegate = self
         
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+        }
+        
         //  Enable the Save button only if the text field has a valid Meal name.
         updateSaveButtonState()
     }

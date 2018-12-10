5989c225d4b105d1f72803141bad6d4da3467aca
juyonglee <juyonglee0208@gmail.com>
2018-12-11 05:12:04 +0900

    데이터 저장을 위한 NSCoding protocol method 구현

--- Food Tracker/Meal.swift
+++ Food Tracker/Meal.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import os.log
 
-class Meal {
+class Meal: NSObject, NSCoding {
     //  MARK: Properties
     var name: String
     var photo: UIImage?
@@ -38,4 +39,28 @@ class Meal {
         self.photo = photo
         self.rating = rating
     }
+    
+    //  MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        //  The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: OSLogType.debug)
+            return nil
+        }
+        
+        //  Because photo is an optional property of Meal, just use conditional case.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+        
+        //  Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
+    }
+    
 }

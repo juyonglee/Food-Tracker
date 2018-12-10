a91af5f07f60bb319e24786bebaf1470b83a8b99
juyonglee <juyonglee0208@gmail.com>
2018-12-11 04:44:36 +0900

    Data Class에 Coding key 선언

--- Food Tracker/Meal.swift
+++ Food Tracker/Meal.swift
@@ -14,6 +14,13 @@ class Meal {
     var photo: UIImage?
     var rating: Int
     
+    //  MARK: Types
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
+    
     //  MARK: Initialization
     init?(name: String, photo: UIImage?, rating: Int) {
         //  The name must not be empty
